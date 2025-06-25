05107a0983b6930197fdbd48b1b2a4461f55f8c9
cccischk <cccischke05@gmail.com>
2025-06-25 14:23:58 -0500

    file setup for QAB

---
+++ QAB/QAB.R
@@ -0,0 +1,18 @@
+# ========================
+# Title:        Quality At-Bat Scoring
+# Description:  Study of improving our view of how to value an at-bat
+# Author:       Cam Cischke
+# Created:      2025-06-25
+# Last Updated: 2025-06-25
+# Dependencies: tidyverse, readr, plate_discipline_v3.R
+# ========================
+
+
+# Import plate discipline classifications 
+source("BaseballSavant/plate_discipline_v3.R")
+
+
+
+### xwOBA Model ----
+
+
